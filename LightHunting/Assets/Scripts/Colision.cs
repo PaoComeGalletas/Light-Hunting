68883f8d4a618b1fcda6b28bd34b4ea3ffefa478
PaoComeGalletas <p.e.b.c.73@gmail.com>
2023-11-19 22:32:55 -0700

    avances nivel 1
    commit cf5824535b598a442d6a68c22b56da811cb044fc
    Author: IvetteValenzuela <Ettevivalenzuela09@gmail.com>
    Date:   Sun Nov 19 00:40:34 2023 -0700
    
        sonido y gemas

--- LightHunting/Assets/Scripts/Colision.cs
+++ LightHunting/Assets/Scripts/Colision.cs
@@ -9,9 +9,19 @@ public class Colision : MonoBehaviour
     private int gem;
     public GameObject txtKey;
     private int key;
+    public GameObject txtMKey;
+    private int Mkey;
     public AudioSource audioSource {get{return GetComponent<AudioSource>();}}
     public AudioClip clipGem;
     public AudioClip clipKey;
+    public string textoAlertaKey = "Press E to take the Key";
+    public string textoAlertaGem = "Press E to take the Gem";
+    public string textoAlertaMKey = "Press E to take the Master Key";
+    public GameObject alerta;
+    public string clave="global";
+    public GameObject gema3;
+
+
    
 
     void Start()
@@ -24,21 +34,71 @@ public class Colision : MonoBehaviour
     {
         
     }
-    public void OnTriggerEnter(Collider objeto){
-        
+    public void OnTriggerStay(Collider objeto){
+
         if(objeto.gameObject.tag=="Gem"){
-         Destroy(objeto.gameObject);
-         audioSource.PlayOneShot(clipGem);
-         gem+=1;
-         txtGem.GetComponent<Text>().text=gem.ToString();   
+            alerta.GetComponent<Text>().text = textoAlertaGem.ToString();
+            alerta.gameObject.SetActive(true);
+
+            if(Input.GetKey("e")){
+                Destroy(objeto.gameObject);
+                audioSource.PlayOneShot(clipGem);
+                gem+=1;
+                txtGem.GetComponent<Text>().text=gem.ToString();   
+                alerta.gameObject.SetActive(false);
+
+                if(gem >= 2){
+                    gema3.SetActive(true);
+                }
+            }
         }
 
         if(objeto.gameObject.tag=="Key"){
-         Destroy(objeto.gameObject);
-         audioSource.PlayOneShot(clipKey);
-         key+=1;
-         txtKey.GetComponent<Text>().text=key.ToString();   
+            alerta.GetComponent<Text>().text = textoAlertaKey.ToString();
+            alerta.gameObject.SetActive(true);
+
+            if(Input.GetKey("e")){
+                this.GetComponent<Inventario>().llaves.Add(clave);
+                Destroy(objeto.gameObject);
+                audioSource.PlayOneShot(clipKey);
+                key+=1;
+                txtKey.GetComponent<Text>().text=key.ToString(); 
+                alerta.gameObject.SetActive(false);
+            }  
+        }
+
+        if(objeto.gameObject.tag=="Master key"){
+            alerta.GetComponent<Text>().text = textoAlertaMKey.ToString();
+            alerta.gameObject.SetActive(true);
+
+            if(Input.GetKey("e")){
+                this.GetComponent<Inventario>().llaves.Add("master key");
+                Destroy(objeto.gameObject);
+                audioSource.PlayOneShot(clipKey);
+                Mkey+=1;
+                txtMKey.GetComponent<Text>().text=Mkey.ToString(); 
+                alerta.gameObject.SetActive(false);
+            }  
         }
         
     }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "Gem" || other.gameObject.tag == "Key" || other.gameObject.tag == "Master key")
+        {
+            alerta.gameObject.SetActive(false);
+        }
+    }
+
+    public void restarLlave(){
+        key-=1;
+        txtKey.GetComponent<Text>().text=key.ToString(); 
+    }
+
+    public void restarLlaveMaestra(){
+        Mkey-=1;
+        txtMKey.GetComponent<Text>().text=Mkey.ToString(); 
+    }
+
+    public int getGems(){return gem;}
 }
